6476d76788d4a96eb1077fd234c7f6866463924e
Hubert Fratczak <hubert.fratczak@onet.com.pl>
2025-10-01 10:20:14 +0200

    to order page to react

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,6 +1,7 @@
 import { Homepage } from './pages/Homepage'
 import { Routes, Route} from 'react-router'
 import {CheckoutPage } from './pages/CheckoutPage'
+import { Orders } from './pages/Orders'
 import './App.css'
 
 
@@ -9,6 +10,7 @@ function App() {
     <Routes>
       <Route index element={<Homepage />} />
       <Route path="checkout" element={<CheckoutPage />}> </Route>
+      <Route path="orders" element={<Orders />} />
     </Routes>
     
   )
