748405e1e20fbecb5102851ded688aa521e00880
Hubert Fratczak <hubert.fratczak@onet.com.pl>
2025-10-04 09:16:28 +0200

    feat: network add axios

--- ecommerce-project/src/pages/Homepage.jsx
+++ ecommerce-project/src/pages/Homepage.jsx
@@ -1,14 +1,13 @@
+import axios from "axios"
 import {Header} from "../components/Header"
 import checkmarkIcon from "../assets/images/icons/checkmark.png"
 import {products} from "../../starting-code/data/products"
 import "./homepage.css"
 export function Homepage() {
-    fetch("http://localhost:3000/api/products"
+    axios.get("http://localhost:3000/api/products"
     ).then((response) => {
-       return response.json()
-    }).then((data) => {
-            console.log(data)
-        })
+       console.log(response.data)
+    })
   return (
     <>
     <title>Ecommerce-project</title>
