c24e2dc46c3ea6d6aa17644671ed707a904791fa
Hubert Fratczak <hubert.fratczak@onet.com.pl>
2025-10-08 14:06:36 +0200

    create unitTests for formatMoney

---
+++ ecommerce-project/src/utils/money.test.js
@@ -0,0 +1,11 @@
+import { formatMoney } from "./money";
+import {it, expect, describe} from "vitest"
+describe("formatMoney", () => {
+    it("formats 1999 cents to 19.99$", () => {
+    expect(formatMoney(1999)).toBe("19.99")
+})
+it("displays 2 decimal", () => {
+    expect(formatMoney(1090)).toBe("10.90")
+    expect(formatMoney(100)).toBe("1.00")
+})
+})
