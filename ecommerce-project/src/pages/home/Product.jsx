bcf0381df21930453d604602102a790b50392785
Hubert Fratczak <hubert.fratczak@onet.com.pl>
2025-10-09 08:54:08 +0200

    create tests for homepage component

--- ecommerce-project/src/pages/home/Product.jsx
+++ ecommerce-project/src/pages/home/Product.jsx
@@ -22,7 +22,8 @@ export function Product({product, loadCart}) {
                    setQuantity(selectedQuantity);
                 }
     return (
-        <div className="product-container">
+        <div className="product-container"
+            data-testid="product-container">
             <div className="product-image-container">
                 <img className="product-image"
                     data-testid="product-image"
